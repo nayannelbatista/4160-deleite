3249928463c858551e6200bc51ff437e653d3212
nayannelbatista <nayannelopes.bd@gmail.com>
2024-12-11 16:29:50 -0300

    Video 1.5 - criando o método de adicionar ao carrinho

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { CartItem } from '../interfaces/cart_item';
+import { Product } from '../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,22 @@ export class CartService {
   private cartItemsSubject = new BehaviorSubject<CartItem[]>([]);
   cartItems$ = this.cartItemsSubject.asObservable();
 
-  private getCurrentItems() {
-    this.cartItemsSubject.getValue();
+  private getCurrentItems(): CartItem[] {
+    return this.cartItemsSubject.getValue();
+  }
+
+  addToCart(product: Product) {
+    const currentItems = this.getCurrentItems();
+    const itemIndex = currentItems.findIndex(
+      (item) => item.product.id === product.id)
+
+      if(itemIndex >= 0) {
+        currentItems[itemIndex].quantity++
+      } else {
+        currentItems.push({product, quantity: 1})
+      }
+
+      this.cartItemsSubject.next(currentItems)
   }
 
 }
